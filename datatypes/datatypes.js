e28144539ddfecde211004a71d66a6a73a82287a
Ahsan Khan <ahsandevsinn@gmail.com>
2025-04-20 12:12:27 +0500

    datatypes

---
+++ datatypes/datatypes.js
@@ -0,0 +1,14 @@
+// number 
+// String 
+// bigint 
+// null 
+// undifiend
+// symbol
+// boolean
+// these are primitive data types
+
+// non primitives data types 
+// object 
+
+let a = "Ahsan";
+console.log(typeof a);
