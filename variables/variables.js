9380344040143101ca097ff2f5846550b112ee42
Ahsan Khan <ahsandevsinn@gmail.com>
2025-04-20 11:40:46 +0500

    variables

--- variables/variables.js
+++ variables/variables.js
@@ -1,6 +1,30 @@
-var n = 5;
-console.log(n);
+// var n = 5;
+// console.log(n);
 
-var n = 30;
-console.log(n);
+// var n = 30;
+// console.log(n);
+
+// let a = 10;
+// console.log(a);
+
+// let b = 30;
+// console.log(b);
+
+
+// const c = 49;
+// console.log(c);
+
+
+const accountId = 103473439
+let accountName = "Ahsan"
+var accountEmail = "a@gmail.com"
+accountState = "Lahore"
+
+
+ accountName = "Ahsan Khan"
+ accountEmail = "ak@gmail.com"
+ accountState = "Pakistan Punjab"
+
+ let password = undefined;
+console.table([accountId,accountName,accountEmail,accountState,password]);
 
