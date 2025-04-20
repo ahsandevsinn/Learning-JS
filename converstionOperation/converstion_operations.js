c91dc305cfa90570d5860e9d743ff633e497c599
Ahsan Khan <ahsandevsinn@gmail.com>
2025-04-20 23:13:16 +0500

    converstion js

---
+++ converstionOperation/converstion_operations.js
@@ -0,0 +1,15 @@
+let score = "33a"
+let valueIntoNumber = Number(score)
+let valueIntoBoolean = Boolean(score)
+let isMale = true
+let isMaleNumber = Number(isMale)
+console.log(score);
+console.log(typeof score);
+console.log(isMaleNumber);
+console.log(typeof isMaleNumber);
+console.log(valueIntoNumber);
+console.log(typeof valueIntoNumber);
+console.log(valueIntoBoolean);
+console.log(typeof valueIntoBoolean);
+
+
