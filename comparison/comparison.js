daafea7504d84e0546a705de706cf6a651fd7069
Ahsan Khan <ahsandevsinn@gmail.com>
2025-04-21 08:28:36 +0500

    comparison

---
+++ comparison/comparison.js
@@ -0,0 +1,16 @@
+// let a = 4;
+// let b = 2;
+
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a*b);
+// console.log(a/b);
+
+
+console.log(1 > "2");
+console.log(1 >= "0");
+console.log(1 < "2");
+console.log(1 <= "2");
+
+console.log(2 === "2");
+
--- converstionOperation/converstion_operations.js
+++ converstionOperation/converstion_operations.js
@@ -1,15 +1,30 @@
-let score = "33a"
-let valueIntoNumber = Number(score)
-let valueIntoBoolean = Boolean(score)
-let isMale = true
-let isMaleNumber = Number(isMale)
-console.log(score);
-console.log(typeof score);
-console.log(isMaleNumber);
-console.log(typeof isMaleNumber);
-console.log(valueIntoNumber);
-console.log(typeof valueIntoNumber);
-console.log(valueIntoBoolean);
-console.log(typeof valueIntoBoolean);
+// let score = "33a"
+// let valueIntoNumber = Number(score)
+// let valueIntoBoolean = Boolean(score)
+// let isMale = true
+// let isMaleNumber = Number(isMale)
+// console.log(score);
+// console.log(typeof score);
+// console.log(isMaleNumber);
+// console.log(typeof isMaleNumber);
+// console.log(valueIntoNumber);
+// console.log(typeof valueIntoNumber);
+// console.log(valueIntoBoolean);
+// console.log(typeof valueIntoBoolean);
+
+/////////////////////////// Operations //////////////////////////
+// let num = 5;
+// let num2  = 8;
+// console.log(num+num2);
+// console.log(num-num2);
+// console.log(num*num2);
+// console.log(num/num2);
+
+console.log(1 + "1");
+console.log("1" + "1");
+console.log("1" + 1 + 1);
+console.log(1 + 1 + "1");
+
+
 
 
